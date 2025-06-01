448fac15d5e54ba4fc7c17967f1630c56720bfce
Lucas Frois <frois.dev@gmail.com>
2025-06-01 20:56:06 -0300

    atualiza build output format

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        dangerouslyAllowSVG: true
-    }
+  output: "standalone",
+  images: {
+    dangerouslyAllowSVG: true,
+  },
 };
 
 export default nextConfig;
